2e99f490c2b8a831755e777d1720c46e5d49f790
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2025-12-18 23:34:40 +0100

    dumb: random-moves minicou

--- minicou/src/bin/cli.rs
+++ minicou/src/bin/cli.rs
@@ -1,8 +1,6 @@
 use ascacou::Board;
 use clap::Parser;
-use std::sync::mpsc;
-use std::thread;
-use std::time::Instant;
+use rand::prelude::IndexedRandom;
 
 #[derive(Parser, Debug)]
 #[clap(about, author, version)]
@@ -20,47 +18,9 @@ struct Args {
 	timeout: u64,
 }
 
-fn run_solver(board: &Board, depth: u8) -> Option<(i16, ascacou::Move, u128)> {
-	match minicou::solve(&board, Some(depth)) {
-		(score, Some(mov), explored_positions) => Some((score, mov, explored_positions)),
-		_ => None,
-	}
-}
-
 fn main() {
 	let args = Args::parse();
-	let t0 = Instant::now();
-	let (tx, rx) = mpsc::channel();
-	let max_depth = args.depth;
-	let board = args.board;
-	thread::spawn(move || {
-		let mut depth = 4;
-		while depth < max_depth {
-			tx.send(
-				run_solver(&board, depth).map(|(eval, mov, explored)| (eval, mov, explored, depth)),
-			)
-			.unwrap();
-			depth += 1;
-		}
-	});
-
-	let mut best_result = None;
-	let timeout = std::time::Duration::from_millis(args.timeout);
-	let mut time_left = timeout.saturating_sub(Instant::now() - t0);
-	while !time_left.is_zero() {
-		if let Ok(result) = rx.recv_timeout(time_left) {
-			best_result = result;
-		}
-		time_left = timeout.saturating_sub(Instant::now() - t0);
-	}
-	let (score, mov, explored_positions, depth) = best_result.expect(&format!(
-		"Could not find a solution. Called with:\n{:#?}",
-		args
-	));
-
+	let moves = args.board.possible_moves().collect::<Vec<_>>();
+	let mov = moves.choose(&mut rand::rng()).unwrap();
 	println!("Move: {}", mov);
-	println!("Time: {:.2?}", Instant::now() - t0);
-	println!("Score: {}", score);
-	println!("Depth: {}/{}", depth, max_depth);
-	println!("Explored positions: {}", explored_positions);
 }
